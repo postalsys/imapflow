0b7c0f38cb909cc4bf1ec4f613fd9ae4858d5aad
Joseph B. Ottinger <joeo@enigmastation.com>
2020-07-08 18:09:11 -0400

    build durability for loggers that may not be 'complete'

--- test/imapflow-test.js
+++ test/imapflow-test.js
@@ -7,3 +7,38 @@ module.exports['Create imapflow instance'] = test => {
     test.ok(imapFlow);
     test.done();
 };
+
+module.exports['Create imapflow instance with custom logger'] = async test => {
+    class CustomLogger {
+        constructor() {}
+
+        debug(obj) {
+            console.log(JSON.stringify(obj));
+        }
+
+        info(obj) {
+            console.log(JSON.stringify(obj));
+        }
+
+        warn(obj) {
+            console.log(JSON.stringify(obj));
+        }
+
+        // eslint-disable-next-line no-unused-vars
+        error(obj) {
+            // we don't actually want to log anything here.
+        }
+    }
+
+    let imapFlow=new ImapFlow({
+        logger:new CustomLogger()
+    });
+    test.ok(imapFlow);
+    try {
+        await imapFlow.connect();
+    } catch(ex) {
+        // it is PERFECTLY okay to have an exception here. We expect an ECONNREFUSED if an exception occurs.
+        test.equal(ex.errno, "ECONNREFUSED");
+    }
+    test.done();
+};
